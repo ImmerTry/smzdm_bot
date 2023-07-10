74c6887a775e0939893a675ad20842e29c72847a
总是尝试 <1136715789@qq.com>
2023-07-10 10:25:08 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=cOS2Ky826gW934KYQPLj; device_id=6139434301669877500339780515eccaac5d01e93e366d1b635c65d0f; homepage_sug=b; r_sort_type=score; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1688951473; _zdmA.vid=*; footer_floating_layer=0; ad_date=10; ad_json_feed=%7B%7D; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22184cc748b5b1158-0bd9613b7018c8-26021e51-2073600-184cc748b5c175%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E5%BC%95%E8%8D%90%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fgithub.com%2F%22%7D%2C%22%24device_id%22%3A%22184cc748b5b1158-0bd9613b7018c8-26021e51-2073600-184cc748b5c175%22%7D; sess=BA-1RsL4OBNbFPhtsmkGAduCsR8jrVUFmHsa9SrFSVz705C8079bkKUYjJNZv9GVYMbdAuUfg1%2BjoTHJD3F1t4feJ00P%2BPZ6N%2BauyNnc35UmqCj3kw0757BYGU%3D; user=user%3A6521775934%7C6521775934; smzdm_id=6521775934; bannerCounter=%5B%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1688955496; _zdmA.uid=ZDMA.oth-Bu57M.1688955497.2419200; _zdmA.time=1688955509307.4444.https%3A%2F%2Fwww.smzdm.com%2F'
 
 
 """
